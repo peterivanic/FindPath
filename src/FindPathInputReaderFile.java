671b5d0f2c4712cc26420da92cc74a74aa3f55e3
Peter Ivanic <peterivanic89@gmail.com>
2022-09-11 20:11:45 +0200

    update FindPathInputReaderFile.java

--- src/FindPathInputReaderFile.java
+++ src/FindPathInputReaderFile.java
@@ -1,7 +1,16 @@
 public class FindPathInputReaderFile extends AbstractFindPathInputReader{
 
+    FindPathInputReaderStdIn findPathInputReaderStdIn = new FindPathInputReaderStdIn();
+
+    @Override
+    public String findPath(char[][] maze) {
+        return null;
+    }
+
+    @Override
     public String findPath(String file) {
         char[][] maze = FindPathUtils.convertFileToCharArray(FindPathUtils.readFileLineByLine(file));
-        return super.findPath(maze);
+        return findPathInputReaderStdIn.findPath(maze);
     }
+
 }
