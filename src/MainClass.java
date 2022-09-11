af0256e8c2558e57692abc4614a8e09bced2d349
Peter Ivanic <peterivanic89@gmail.com>
2022-09-11 20:32:45 +0200

    update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,14 @@
-import java.util.Arrays;
 
 public class MainClass {
     public static void main(String[] args) {
 
-       char[][] r =  FindPathUtils.convertFileToCharArray(
-               FindPathUtils.readFileLineByLine("C:\\JAVA projects\\Find path app\\src\\maze.txt")
-       );
+       char[][] maze =  FindPathUtils.convertFileToCharArray(
+               FindPathUtils.readFileLineByLine("C:\\JAVA projects\\Find path app\\src\\maze.txt"));
 
-        System.out.println(Arrays.toString(FindPathUtils.findStartOrEnd(r, 'X')));
+       AbstractFindPathInputReader findPathInputReaderStdIn = new FindPathInputReaderStdIn();
+        AbstractFindPathInputReader findPathInputReaderFile = new FindPathInputReaderFile();
 
+        System.out.println(findPathInputReaderStdIn.findPath(maze,null));
+        System.out.println(findPathInputReaderFile.findPath(null,"C:\\JAVA projects\\Find path app\\src\\maze.txt"));
     }
 }
